385bfc53b9ed7c9157cde13aa5404edd1704ebe8
lyqluis <woyaozhucelushi@163.com>
2023-06-05 13:58:50 +0800

    feat: fav_content & user_info

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,29 @@
 const axios = require('axios')
 const router = require('koa-router')()
 
+// ## 用户名片信息
+// mid, user's mid
+router.get('/user_info', async ctx => {
+  const { cookie } = ctx.headers
+  const { mid } = ctx.query
+
+  const res = await axios({
+    method: 'get',
+    url: 'https://api.bilibili.com/x/web-interface/card',
+    params: {
+      mid,
+      // photo: false, // 请求用户主页头图，默认 false
+    },
+    headers: {
+      cookie
+    },
+  })
+  ctx.body = res.data
+})
+
 // ## 用户状态数值
 // vmid, user's mid
-router.get('/userstat', async (ctx, next) => {
+router.get('/user_stat', async (ctx, next) => {
   const { cookie } = ctx.headers
   const { vmid } = ctx.query
 
@@ -51,6 +71,34 @@ router.get('/fav_info', async ctx => {
     url: 'https://api.bilibili.com/x/v3/fav/folder/info',
     params: {
       media_id,
+    },
+    headers: {
+      cookie
+    },
+  })
+  ctx.body = res.data
+})
+
+// ## 收藏夹内容列表
+// media_id, num, 目标收藏夹 mlid（完整 id）	必要
+// ps, num, 每页数量	必要	定义域：1 - 20
+// tid, num, 分区 tid	非必要	默认为全部分区，0：全部分区
+// keyword, str, 搜索关键字	非必要
+// order, str, 排序方式	非必要	按收藏时间：mtime，按播放量：view，按投稿时间：pubtime
+// type, num, 查询范围	非必要	0：当前收藏夹（对应 media_id）1：全部收藏夹
+// pn, num, 页码	非必要	默认为 1
+// platform, str, 平台标识	非必要	可为 web（影响内容列表类型）
+router.get('/fav_content', async ctx => {
+  const { cookie } = ctx.headers
+  const { media_id, ps, pn = 1 } = ctx.query
+
+  const res = await axios({
+    method: 'get',
+    url: 'https://api.bilibili.com/x/v3/fav/resource/list',
+    params: {
+      media_id,
+      ps,
+      pn
     },
     headers: {
       cookie
