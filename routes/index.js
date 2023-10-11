e65387149230e8ebabdcd462431165e1b712f69e
lyqluis <woyaozhucelushi@163.com>
2023-10-11 21:53:25 +0800

    fix: deploy vercel

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,25 @@
-const router = require('koa-router')()
+const router = require("koa-router")()
 const path = require("path")
-const { loadFilesFromDir } = require('../utils/file')
+const { loadFilesFromDir } = require("../utils/file")
 
-router.get('/', async (ctx, next) => {
-  ctx.body = 'success'
-  next()
+router.get("/", async (ctx, next) => {
+	ctx.body = "success"
+	next()
 })
+
+// must require all files staticly, otherwise vercel doesn't work
+require("./indexAll")
 
 /**
  * @param {Array} files files array
  */
 function useAll(files) {
-  files.forEach((route) => {
-    router.use("", route.file.routes(), route.file.allowedMethods())
-  })
+	files.forEach((route) => {
+		router.use("", route.file.routes(), route.file.allowedMethods())
+	})
 }
 
 const files = loadFilesFromDir(path.join(__dirname, "./"), false)
 useAll(files)
 
 module.exports = router
-
--- utils/file.js
+++ utils/file.js
@@ -1,8 +1,8 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs")
+const path = require("path")
 
 /**
- * @func: 
+ * @func:
  * @param {String} path
  * @return {Object} path info obejct
  * - dir <string>
@@ -11,7 +11,7 @@ const path = require('path')
  * - name <string>
  * - ext <string>
  */
-const getPathInfo = p => path.parse(p)
+const getPathInfo = (p) => path.parse(p)
 
 /**
  * @description 递归读取文件，类似于 webpack 的 require.context()
@@ -20,39 +20,41 @@ const getPathInfo = p => path.parse(p)
  * @param {Array} extList 查询文件后缀，默认 ['.js']
  * @param {Array} excludes 排除文件，默认 ['index']
  * @return {[Object]} files array
-*/
+ */
 const loadFilesFromDir = (
-  directory,
-  useSubdir = false,
-  extList = [".js"],
-  excludes = ['index']
+	directory,
+	useSubdir = false,
+	extList = [".js"],
+	excludes = ["index", "indexAll"]
 ) => {
-  const filesList = []
-  // 递归读取文件
-  function readFileList(directory, useSubdir, extList, excludes) {
-    const files = fs.readdirSync(directory) // 同步读取目录
-    files.forEach((item) => {
-      const fullPath = path.join(directory, item)
-      const stat = fs.statSync(fullPath)  // 文件信息对象
-      // 递归子目录
-      if (stat.isDirectory() && useSubdir) {
-        readFileList(path.join(directory, item), useSubdir, extList, excludes)
-      } else {
-        const info = getPathInfo(fullPath)
-        !excludes.includes(info.name) && extList.includes(info.ext) && filesList.push({
-          path: fullPath,
-          file: require(fullPath),
-          ...info,
-        })
-      }
-    })
-  }
-  readFileList(directory, useSubdir, extList, excludes)
-  
-  return filesList
+	const filesList = []
+	// 递归读取文件
+	function readFileList(directory, useSubdir, extList, excludes) {
+		const files = fs.readdirSync(directory) // 同步读取目录
+		files.forEach((item) => {
+			const fullPath = path.join(directory, item)
+			const stat = fs.statSync(fullPath) // 文件信息对象
+			// 递归子目录
+			if (stat.isDirectory() && useSubdir) {
+				readFileList(path.join(directory, item), useSubdir, extList, excludes)
+			} else {
+				const info = getPathInfo(fullPath)
+				!excludes.includes(info.name) &&
+					extList.includes(info.ext) &&
+					filesList.push({
+						path: fullPath,
+						file: require(fullPath),
+						...info,
+					})
+			}
+		})
+	}
+	readFileList(directory, useSubdir, extList, excludes)
+
+	return filesList
 }
 
 module.exports = {
-  getPathInfo,
-  loadFilesFromDir
+	getPathInfo,
+	loadFilesFromDir,
 }
